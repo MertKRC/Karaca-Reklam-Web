a3a0999658fb72ce929629a8aed508e3b74ee18e
Mert KARACA <47756576+MertKRC@users.noreply.github.com>
2019-11-26 20:38:13 +0300

    Created contact form's backend as skeleton. Still have some issues. Hotfix will come soon

--- Karaca-Reklam-Web/Controllers/iletisimController.cs
+++ Karaca-Reklam-Web/Controllers/iletisimController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Karaca_Reklam_Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +10,24 @@ namespace Karaca_Reklam_Web.Controllers
 {
     public class iletisimController : Controller
     {
-        // GET: iletisim
+        //GET: iletisim
         public ActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = new StringBuilder();
+                body.AppendLine("Ad & Soyad: " + model.Name);
+                body.AppendLine("E-Mail Adresi: " + model.Email);
+                body.AppendLine("Konu: " + model.Subject);
+                body.AppendLine("Mesaj: " + model.Message);
+                Mail.MailSender(body.ToString());
+                ViewBag.Success = true;
+            }
             return View();
         }
     }
---
+++ Karaca-Reklam-Web/Models/Mail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+
+namespace Karaca_Reklam_Web.Models
+{
+    public class Mail
+    {
+        //Mail sender function start
+        public static void MailSender(string body)
+        {
+            var fromAddress = new MailAddress("mrtkrc41@gmail.com");
+            var toAddress = new MailAddress("mrtkrc41@gmail.com");
+            const string subject = "Karaca Yazılım | Sitenizden Gelen İletişim Formu";
+            using (var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, "password cencored for github")
+            })
+            {
+                using (var message = new MailMessage(fromAddress, toAddress) { Subject = subject, Body = body })
+                {
+                    smtp.Send(message);
+                }
+            }
+        }
+        //Mail sender function end
+    }
+
+}
